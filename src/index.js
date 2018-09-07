165f64f643644bb870134762ac9d6120e2d1a43f
Robert <robertxluo@gmail.com>
2018-09-06 17:26:01 -0700

    Complete filters and notes modules for webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import { getNotes, createNote } from "./notes.js";
+import { getNotes, createNote, removeNote, updateNote } from "./notes.js";
+import { getFilters, setFilters } from "./filters.js";
 
-console.log(getNotes());
-createNote();
-console.log(getNotes());
+// updateNote("8ba2c788-2e4b-44d7-b499-1667d66521f0", {
+//     "title": "My note title",
+//     "body": "Woah this is cool!211"
+// })
+// console.log(getNotes());
+console.log(getFilters());
+
